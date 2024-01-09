52c0dd29aa208a3cf9e54140b2945616320035b0
PavlenkoOksana <pavlenko.oksana.od@gmail.com>
2024-01-09 22:29:34 +0100

    09.01.2024-1

--- task_2.py
+++ task_2.py
@@ -3,33 +3,25 @@ def binary_search(arr, x):
     low = 0
     high = len(arr) - 1
     mid = 0
- 
+
     while low <= high:
         iter += 1
         mid = (high + low) // 2
- 
-        # якщо x більше за значення посередині списку, ігноруємо ліву половину
-        if arr[mid] < x:
+
+        if arr[mid] == x:
+            return (iter, arr[mid])
+        elif arr[mid] < x:
             low = mid + 1
- 
-        # якщо x менше за значення посередині списку, ігноруємо праву половину
-        elif arr[mid] > x:
-            high = mid - 1
- 
-        # інакше x присутній на позиції і повертаємо його
         else:
-            if mid < len(arr) - 1:
-                return (iter, arr[mid+1])
-            else:
-                return (iter, 'відсутня "верхня межа"')
- 
-    # якщо елемент не знайдений
-    return -1
+            high = mid - 1
+
+    if low < len(arr):
+        return (iter, arr[low])
+    else:
+        return (iter, arr[-1])
+
 
 arr = [1/40, 1/10, 1/4, 1/3, 1/2]
-x = 1/3
+x = 1/6
 result = binary_search(arr, x)
-if result != -1:
-    print(f"Element is present at index {result}")
-else:
-    print("Element is not present in array")
+print(result)
